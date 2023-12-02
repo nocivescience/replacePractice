f546857ff02ed46908e32e7dd41060da8f9f7c9c
nocive.science <nocive.science@gmail.com>
2023-12-02 03:34:15 -0300

    archivos totalmente limpios

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
 // let palabra= prompt("Ingrese una palabra");
-let palabra = "hola";
-console.log(palabra.replace(/a/g, "4").replace(/e/g, "3").replace(/i/g, "1").replace(/o/g, "0").replace(/u/g, "2"));
