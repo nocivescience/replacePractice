ad95734cf62c193f748db2b877f681135a4adf55
nocive.science <nocive.science@gmail.com>
2023-12-02 18:02:03 -0300

    /\btu\b/g /tu/g /\btu/g /tu\b/g

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ const precio1 = frase1.replace();
 const frase2= "foobar foo barfoo foo-bar foo123"; // reemplazar foo por bar
 const otroReemplazo2 = frase2.replace(/\bfoo\b/g, 'bar');  // \b es para buscar la palabra exacta
 h1Element[4].innerHTML = otroReemplazo2; // barbar bar barbar bar-bar barbar123
+
+const frase3= "tu tutsi tu tutotulo tute tu tutetuo tuto"; // reemplazar tu por za
+const otroReemplazo3 = frase3.replace(/\btu\b/g, 'za');  // \b es para buscar la palabra exacta
+h1Element[5].innerHTML = otroReemplazo3; // za tutsi tutotulo tute tuteo tuto
+const otroReemplazo4 = frase3.replace(/tu/g, 'za');  // \b es para buscar la palabra exacta
+h1Element[6].innerHTML = otroReemplazo4; // za tutsi za zaetulo za zaetuo za
+
+const frase4= "tu situ tugde dertuyt tu"; // reemplazar la fecha por 2021-08-23
+const otroReemplazo5 = frase3.replace(/\btu/g, 'za');  // \b es para buscar la palabra exacta
+h1Element[7].innerHTML = otroReemplazo5; // za tutsi za zaetulo za zaetuo za
+const otroReemplazo6 = frase3.replace(/tu\b/g, 'za');  // \b es para buscar la palabra exacta
+h1Element[8].innerHTML = otroReemplazo6; // za tutsi za zaetulo za zaetuo za
+
+const frase5= "la fecha es para 23 de 08 de 2021 cambia q56ue"; // reemplazar la fecha por 2021-08-23
